ec914affcbc3bad929a024052ac18a99d26fcc9d
tiwaririjul <tiwaririjul7@gmail.com>
2022-12-31 02:18:39 +0530

    tesla

---
+++ src/components/Home.js
@@ -0,0 +1,68 @@
+import React from "react";
+import styled from "styled-components";
+import Section from "./Section";
+
+const Home = () => {
+  return (
+    <Container>
+      <Section
+        title="Model S"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-s.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Model Y"
+        description="Order Online for Touchless Delivery"
+        // backgroundImg="model-y.jpg"
+        backgroundImg="model-s.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Model S"
+        description="Order Online for Touchless Delivery"
+        // backgroundImg="model-3.jpg"
+        backgroundImg="model-s.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Model S"
+        description="Order Online for Touchless Delivery"
+        // backgroundImg="model-x.jpg"
+        backgroundImg="model-s.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Lowest Cost Solar Panels in America"
+        description="Money back guatrantee"
+        // backgroundImg="solar-panel.jpg"
+        backgroundImg="model-s.jpg"
+        leftBtnText="Order order"
+        rightBtnText="Learn more"
+      />
+      <Section
+        title="Solar for New Roofs"
+        description="SolarRoof Costs less then your pocket money"
+        backgroundImg="solar-roof.jpg"
+        leftBtnText="Order now"
+        rightBtnText="Learn more"
+      />
+      <Section
+        title="Accessories"
+        description=""
+        backgroundImg="accessories.jpg"
+        leftBtnText="Shop now"
+      />
+    </Container>
+  );
+};
+
+export default Home;
+
+const Container = styled.div`
+  height: 100vh;
+`;
---
+++ src/components/Section.js
@@ -0,0 +1,88 @@
+import React from "react";
+import styled from "styled-components";
+import Fade from "react-reveal/Fade";
+
+const Section = (props) => {
+  return (
+    <Wrap bgImage={props.backgroundImg}>
+      <Fade bottom>
+        <ItemText>
+          <h1>{props.title}</h1>
+          <p>{props.description}</p>
+        </ItemText>
+      </Fade>
+
+      <Buttons>
+        <Fade bottom>
+          <ButtonGroup>
+            <LeftButton>{props.leftBtnText}</LeftButton>
+            {props.rightBtnText && (
+              <RightButton>{props.rightBtnText}</RightButton>
+            )}
+          </ButtonGroup>
+        </Fade>
+        <DownArrow src="/images/down-arrow.svg" />
+      </Buttons>
+    </Wrap>
+  );
+};
+
+export default Section;
+
+const Wrap = styled.div`
+  z-index: 10;
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  backgrond-position: center;
+  background-repeat: no-repeat;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  background-image: ${(props) => `url("/images/${props.bgImage}")`};
+`;
+
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
+
+// background-image: url("/images/model-s");
+
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 8px;
+`;
+const RightButton = styled(LeftButton)`
+  background-color: white;
+  opacity: 0.65s;
+  color: black;
+`;
+
+const DownArrow = styled.img`
+  height: 40px;
+  overflow-x : hidden
+  animation : animateDown infinite 1.5s
+`;
+
+const Buttons = styled.div``;
